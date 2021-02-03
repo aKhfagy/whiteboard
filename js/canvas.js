419ba86f06f6f69c1a605e99f35873ac7fadd37c
aKhfagy <52210948+aKhfagy@users.noreply.github.com>
2021-02-03 14:47:40 +0200

    Removed comments

--- js/canvas.js
+++ js/canvas.js
@@ -90,14 +90,3 @@ window.addEventListener('load', () => {
         alert("Size of stroke is " + lineWidthvar);
     });
 });
-
-// function myFunction() {
-// var txt;
-// var person = prompt("Please enter your name:", "Harry Potter");
-// if (person == null || person == "") {
-//     txt = "User cancelled the prompt.";
-// } else {
-//     txt = "Hello " + person + "! How are you today?";
-// }
-// document.getElementById("demo").innerHTML = txt;
-// }
